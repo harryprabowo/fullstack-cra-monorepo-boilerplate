2823b9dd4680fe9d87f28fb9754799696155295b
Harry Prabowo <harryprabowo10@gmail.com>
2020-05-09 05:03:39 +0700

    Add babel plugin root import

--- packages/client/config-overrides.js
+++ packages/client/config-overrides.js
@@ -1,4 +1,18 @@
-const rewireYarnWorkspaces = require('react-app-rewire-yarn-workspaces');
-module.exports = function override(config, env) {
-    return rewireYarnWorkspaces(config, env);
-};
+const {
+    override,
+    addBabelPlugin,
+} = require("customize-cra");
+
+module.exports = override(
+    addBabelPlugin([
+        "babel-plugin-root-import",
+        {
+            paths: [
+                {
+                    rootPathSuffix: "src",
+                    rootPathPrefix: "~"
+                }
+            ]
+        }
+    ]),
+)
